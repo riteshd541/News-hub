44d1c3e49e00d0212ebb64ae6a308cfceee731de
riteshd541 <riteshd541@gmail.com>
2023-08-06 23:36:04 +0530

    update env again

--- src/components/FirebaseAuth/firebase.js
+++ src/components/FirebaseAuth/firebase.js
@@ -1,15 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBS6jsBrLGCd9JCHuWXJhl-edOwbKsiPTc",
-//   authDomain: "authentication-6dab2.firebaseapp.com",
-//   projectId: "authentication-6dab2",
-//   storageBucket: "authentication-6dab2.appspot.com",
-//   messagingSenderId: "477324575747",
-//   appId: "1:477324575747:web:258b48972c62641c66ba36",
-//   measurementId: "G-63CML6179M",
-// };
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
